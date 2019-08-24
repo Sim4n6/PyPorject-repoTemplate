aa124ee5817acbfda975f1ab5bb6a12bc6785044
Sim4n6 <sim4n6@gmail.com>
2019-08-24 13:10:08 +0100

    added a sample substract func 3

--- product1/product.py
+++ product1/product.py
@@ -1,2 +1,6 @@
 def inc(x):
     return x + 1
+
+
+def sub(a, b):
+    return a - b
--- tests/test_inc.py
+++ tests/test_inc.py
@@ -1,9 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from product1.product import inc
-
 
 
 def test_answer():
